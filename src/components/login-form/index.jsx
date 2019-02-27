8ac7fa1769aa4f4087d7267929279d283fa8979a
e <dhxiao@halv.com>
2019-02-27 09:11:03 +0800

    '完成登录表单验证'

---
+++ src/components/login-form/index.jsx
@@ -0,0 +1,76 @@
+import React,{Component} from 'react'
+import {
+  Form, Icon, Input, Button,message
+} from 'antd';
+const {Item}=Form
+
+
+
+ class FormLogin extends Component{
+
+   changePassword=(rule,value,callback)=>{
+    if(!value){
+      callback('用户密码不能为空')
+    }else if(value.length<4){
+      callback('密码长度最小为4位')
+    }else if (value.length>16){
+      callback('密码长度最大为16位')
+    }else if (!/^[a-zA-Z0-9_]+$/.test((value))){
+      callback('用户名不能有特殊字符，只能为大小写字母数字和下划线')
+    }else {
+      callback()
+    }
+   }
+
+   handleSubmit=(e)=>{
+     e.preventDefault()
+     const {validateFields,resetFields}=this.props.form
+     validateFields((error,values)=>{
+       if(!error){
+
+       }else {
+         resetFields(['password']);
+       const err= Object.values(error).reduce((prev,curr)=> prev+curr.errors[0].message,'');
+         message.error(err)
+       }
+     })
+
+
+
+ }
+  render(){
+    const {getFieldDecorator}=this.props.form
+
+    return(
+      <Form className="login-form" onSubmit={this.handleSubmit}>
+        <Item>
+          {getFieldDecorator(
+            'username',{
+              rules:[
+                {required:true,message:'请输入用户名'},
+                {max:16,message:'用户名的最大长度为16位'},
+                {min:4,message:'用户名的最小长度为4位'},
+                {pattern:/^[a-zA-Z0-9_]+$/,message:'用户名不能有特殊字符，只能为大小写字母数字和下划线'}
+                ]
+            }
+          )(<Input prefix={<Icon type="user" />} placeholder="用户名" />)}
+        </Item>
+        <Item>
+          {getFieldDecorator(
+            'password',{
+              rules:[{validator:this.changePassword}]
+            }
+          )
+          ( <Input prefix={<Icon type="lock" />} type="password"  placeholder="密码" />)}
+        </Item>
+        <Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">登录</Button>
+        </Item>
+      </Form>
+    )
+  }
+}
+
+const   WrappedNormalLoginForm=Form.create()(FormLogin)
+
+export default WrappedNormalLoginForm
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,10 +2,8 @@ import React,{Component}from 'react'
 import './bg.jpg'
 import logo from './logo.png'
 import './index.less'
-import {
-  Form, Icon, Input, Button,
-} from 'antd';
-const {Item}=Form
+import FormLogin from '../../components/login-form/index'
+
 export default class Login extends Component{
   render(){
     return (
@@ -16,18 +14,7 @@ export default class Login extends Component{
         </header>
         <div className="login-content">
           <h2>用户登陆</h2>
-          <Form className="login-form">
-            <Item>
-              <Input prefix={<Icon type="user" />} placeholder="用户名" />
-            </Item>
-          <Item>
-            <Input prefix={<Icon type="lock" />} type="password" placeholder="密码" />
-          </Item>
-            <Item>
-              <Button type="primary" className="login-form-button">登录</Button>
-            </Item>
-
-          </Form>
+           <FormLogin/>
         </div>
       </div>
     )
